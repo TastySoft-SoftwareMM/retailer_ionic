6bf22bddc8085e50cb652df2cbc77cfa3f833012
WaiYanSoeGithubMyanmar <waiyansoe4421@gmail.com>
2021-02-10 21:48:20 +0630

    upload files

--- src/app/services/intercom.service.ts
+++ src/app/services/intercom.service.ts
@@ -45,15 +45,15 @@ export class IntercomService {
       // localStorage.setItem("imgurl", "http://52.255.142.115:8084/");
 
       // For Customer_Testing
-      this.apiurl = "http://52.253.88.71:8084/madbrepository/";
-      localStorage.setItem("url", "http://52.253.88.71:8084/madbrepository/");
-      localStorage.setItem("imgurl", "http://52.253.88.71:8084/");
+      // this.apiurl = "http://52.253.88.71:8084/madbrepository/";
+      // localStorage.setItem("url", "http://52.253.88.71:8084/madbrepository/");
+      // localStorage.setItem("imgurl", "http://52.253.88.71:8084/");
 
 
       //--- Go liver server for dec7      
-      // this.apiurl = "http://18.136.44.90:8084/madbrepository/";
-      // localStorage.setItem("url", "http://18.136.44.90:8084/madbrepository/");
-      // localStorage.setItem("imgurl", "http://18.136.44.90:8084/");
+      this.apiurl = "http://18.136.44.90:8084/madbrepository/";
+      localStorage.setItem("url", "http://18.136.44.90:8084/madbrepository/");
+      localStorage.setItem("imgurl", "http://18.136.44.90:8084/");
 
 
       // http://18.136.44.90:8084/madbrepository/
